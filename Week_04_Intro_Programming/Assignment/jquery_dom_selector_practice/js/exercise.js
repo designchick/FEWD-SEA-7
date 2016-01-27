5904ceea0c023f92eab3b98775886637405405e5
designchick <jamiecab@dlkc6436595s-MacBook-Pro.local>
2016-01-27 01:08:45 -0800

    Completed JQuery Selector Challenge

--- Week_04_Intro_Programming/Assignment/jquery_dom_selector_practice/js/exercise.js
+++ Week_04_Intro_Programming/Assignment/jquery_dom_selector_practice/js/exercise.js
@@ -13,11 +13,12 @@
  * We've started you off, replace the sec
  */
  
-	function question1(){
-		$("replaceMe").css("replaceMe", "replaceMe");
-	}
+	// function question1(){
+	// 	$("p").css("color", "blue");
+	// }
 
-	$("#button1").click(question1);
+	// $("#button1").click(question1);
+
 
 /**
  * Question 2:
@@ -25,7 +26,7 @@
  */
 
 	// function question2(){
-	// 	$("replaceMe").replaceMe("replaceMe");
+	// 	$("h2").replaceWith("Jamie");
 	// }
 
 	// $("#button1").click(question2);
@@ -36,8 +37,8 @@
  * Using jQuery change the content to read "True Fact".
  */
 
-	// function replaceMe(){
-	// 	$("replaceMe").html("replaceMe");
+	// function question3(){
+	// 	$(".falseFact").replaceWith("True Fact");
 	// }
 
 	// $("#button1").click(question3);
@@ -48,20 +49,32 @@
  * Question 4: 
  * When button 2 is clicked, change the background color of the whole page to "pink"
  */
+	// function question4(){
+	// 	$("body").css("background-color", "pink");
+	// }
 
-
+	// $("#button2").click(question4);
 
 /**
  * Question 5:
  * When button 2 is clicked, change the color of all h2's to "green"
  */
+	// function question4(){
+	// 	$("h2").css("color", "green");
+	// }
 
+	// $("#button2").click(question4);
 
 
 /**
  * Question 6:
  * When button 2 is clicked, change the html content of all "blockquotes" to be "<span>no quote</span>"
  */
+	// function question6(){
+	// 	$("blockquote").html("<span>no quote</span>");
+	// }
+
+	// $("#button2").click(question6);
 
 
 //SECTION 3: This section is more difficult. Try your hand at the next 3 questions.
@@ -70,13 +83,25 @@
  * Question 7:
  * When button 3 is clicked, change the text in the header's h1 tag to read "jQuery Ninja"
  */
+	// function question7(){
+	// 	$("h1").html("jQuery Ninja");
+	// }
 
+	// $("#button3").click(question7);
 
 /**
  * Question 8:
  * Have the following code execute when button 3 is clicked. Write a JavaScript comment with a description of what happened. 
  * $(".city").attr("src", "http://lorempixel.com/g/500/400/food");
  */
+
+ // The photo changed to sushi 
+
+	// function question8(){
+ // 		$(".city").attr("src", "http://lorempixel.com/g/500/400/food");
+	// 	}
+
+	// 	$("#button3").click(question8);
 
 
 /**
@@ -85,7 +110,9 @@
  * Bonus: Try to do this with only one jQuery call. $("selector").css({attribute: "value", attribute: "value"});
  */
 
+ 	function question9(){
+		$("p").css({color: "blue", "font-family": "Georgia"});
+	}
 
-
-
+	$("#button3").click(question9);
 
